27cf6c27a4fc036de9c0d62f6718886619d0020d
Daffy Duck <ayos888@gmail.com>
2020-07-08 21:09:12 +0800

    new updates

--- SportsStore.Domain/Abstract/IProductsRepository.cs
+++ SportsStore.Domain/Abstract/IProductsRepository.cs
@@ -1,9 +1,9 @@
-﻿using SportsStore.Domain.Entities;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.Domain.Abstract
 {
-    public interface IProductsRepository
+    public interface IProductRepository
     {
         IEnumerable<Product> Products { get; }
     }
--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SportsStore.Domain.Entities
 {
-    class Product
+    public class Product
     {
         public int ProductID { get; set; }
         public string Name { get; set; }
